1726a43ee796f4c39846f103579336c84fa1cf10
Jedrzej Winnicki <jwinnicki2@gmail.com>
2019-12-23 16:09:25 +0100

    adding first phase

---
+++ src/components/SettingsComponent/SettingsComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './SettingsComponent.scss';
+
+const SettingsComponent = () => {
+    return (
+        <div className='SettingsComponent'>SettingsComponent</div>
+    );
+}
+
+export default SettingsComponent;
--- src/containers/FirstPhaseMain/FirstPhaseMain.js
+++ src/containers/FirstPhaseMain/FirstPhaseMain.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import './FirstPhaseMain.scss';
+import SettingsComponent from '../../components/SettingsComponent/SettingsComponent';
+
 
 const FirstPhaseMain = () => {
     return (
-        <div className='FirstPhaseMain'>FirstPhaseMain</div>
+        <div className='FirstPhaseMain'>
+            <div className='FirstPhaseMain-settings'>
+                <SettingsComponent />
+            </div>
+            <div className='FirstPhaseMain-composition'></div>
+        </div>
     );
 }
 
