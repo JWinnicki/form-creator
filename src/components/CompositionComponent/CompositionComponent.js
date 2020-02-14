e3704c96067cccbfc8c74e93fd1ad3ed502b9e9e
unknown <jwinnicki2@gmail.com>
2020-02-14 17:29:24 +0100

    component refactor

--- src/components/CompositionComponent/CompositionComponent.js
+++ src/components/CompositionComponent/CompositionComponent.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import './CompositionComponent.scss';
-import DummyTextInput from '../DummyComponents/DummyTextInput/DummyTextInput';
-import Icon from '../Icon/Icon';
 import DummyField from '../DummyField/DummyField';
 
 const CompositionComponent = props => {
@@ -28,51 +26,6 @@ const CompositionComponent = props => {
         if(title) {
             return <h1 style={headerStyle} className='CompositionComponent-title' >{title}</h1>;
         }
-    }
-
-    const renderFileds = () => {
-        return formFields.map(el => {
-            if(el.fieldType === 'text') {
-                const elLabelStyle = {
-                    color: el.textInputLabelFontColor,
-                    fontSize: `${el.textInputLabelFontSize}px`,
-                    fontWeight: el.textInputLabelFontWeight,
-                    fontStyle: el.textInputLabelFontStyle    
-                }
-
-                return (
-                    <div key={el.fieldId} className='CompositionComponent-textInputDiv'>
-                        <div>
-                            <p style={elLabelStyle}>{el.textInputLabel}</p>
-                        </div>
-                        <DummyTextInput 
-                            textInputWidth={el.textInputWidth}
-                            textInputBackground={el.textInputBackground}
-                            textInputFontSize={el.textInputFontSize}
-                            textInputFontColor={el.textInputFontColor}
-                            inputPlaceholder={el.inputPlaceholder}
-                        />
-                        <div className='CompositionComponent-fieldControls' style={{backgroundColor: backgroundColor}}>
-                            <div className='CompositionComponent-deleteDiv'>
-                                <button type='button' className='CompositionComponent-controlsButton'>
-                                    <Icon icon='delete' size='tiny' inverted='inverted' backgroundColor={backgroundColor}/>
-                                </button>
-                            </div>
-                            <div className='CompositionComponent-arrowsDiv'>
-                                <button type='button' className='CompositionComponent-controlsButton'>
-                                    <Icon icon='arrow-right' size='tiny' rotate='deg270' inverted='inverted' backgroundColor={backgroundColor} />
-                                </button>
-                                <button type='button' className='CompositionComponent-controlsButton'>
-                                    <Icon icon='arrow-right' size='tiny' rotate='deg90' inverted='inverted' backgroundColor={backgroundColor} />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                )
-            } else {
-                return <div>in progress</div>
-            }
-        })
     }
 
     const renderFields = () => {
