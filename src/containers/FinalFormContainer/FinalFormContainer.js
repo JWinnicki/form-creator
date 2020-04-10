7916758b8fa954496d31b4183272f2b76d70108d
unknown <jwinnicki2@gmail.com>
2020-04-10 17:55:30 +0200

    adding final form component

--- src/containers/FinalFormContainer/FinalFormContainer.js
+++ src/containers/FinalFormContainer/FinalFormContainer.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import './FinalFormContainer.scss'
+import MainButton from '../../components/MainButton/MainButton';
+import { FormContext } from '../../context/form-context';
 
 const FinalFormContainer = props => {
+
+    const { contextFormData } = useContext(FormContext);
+
+    useEffect(() => {
+        if(!contextFormData.formBackgroundData) {
+            props.history.push('/');
+        }
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const buttonStyle = {
+        buttonDimensionsOption: 'fixed',
+        buttonHeight: 5,
+        buttonWidth: 16,
+        fontColor: '#ffffff',
+        primaryColor: '#cf0000',
+        secondaryColor: '#000000'
+    }
+
     return (
-        <div className='FinalFormContainer'>Final</div>
+        <div className='FinalFormContainer'>
+            <div className=''>
+                <Link to='/create-form'>
+                    <MainButton styleData={buttonStyle} unit='em'>Back</MainButton>
+                </Link>
+            </div>
+        </div>
     )
 }
 
